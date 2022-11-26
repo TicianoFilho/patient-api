79f3ab1abf8f57fdc85ef1af3998fe1d8da021d7
TicianoFilho <ticianofilho@gmail.com>
2022-11-26 13:58:51 -0300

    feat: add DependenteEntity

---
+++ src/main/java/com/globalhealth/estagio/patientapi/model/entity/DependenteEntity.java
@@ -0,0 +1,9 @@
+package com.globalhealth.estagio.patientapi.model.entity;
+
+import javax.persistence.Entity;
+
+@Entity(name = "dependente")
+public class DependenteEntity extends Paciente {
+	
+
+}
