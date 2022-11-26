f095c2d30e7308d599ee96d69cc8e0dc2e3ca46b
TicianoFilho <ticianofilho@gmail.com>
2022-11-26 16:08:34 -0300

    refactor: add validation to DependenteDto

---
+++ src/main/java/com/globalhealth/estagio/patientapi/dto/PacienteDto.java
@@ -0,0 +1,28 @@
+package com.globalhealth.estagio.patientapi.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
+
+import lombok.Data;
+
+@Data
+public abstract class PacienteDto {
+
+	private Long id;
+	
+	@NotBlank(message = "O nome não pode ser vazio.")
+	@Length(min = 5, max = 200, message = "O nome deve conter entre 5 a 200 caracteres.")
+	private String nome;
+	
+	@NotBlank(message = "O CPF não pode ser vazio.")
+	@CPF(message = "CPF inválido.")
+	private String cpf;
+	
+	@NotBlank(message = "O código do cartão não pode ser vazio.")
+	private String codigoCartao;
+	
+	private boolean active;
+	
+}
