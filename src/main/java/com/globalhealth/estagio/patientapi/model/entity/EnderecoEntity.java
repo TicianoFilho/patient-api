37faa1c1031fa8dda7d621484f0b06ef8cb6ff34
TicianoFilho <ticianofilho@gmail.com>
2022-11-26 13:52:13 -0300

    other: update project

---
+++ src/main/java/com/globalhealth/estagio/patientapi/model/entity/EnderecoEntity.java
@@ -0,0 +1,35 @@
+package com.globalhealth.estagio.patientapi.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "endereco")
+public class EnderecoEntity extends AppBaseEntityClass{
+
+	@Column(nullable = false)
+	private String rua;
+	
+	@Column(nullable = false)
+	private String cidade;
+	
+	@Column(nullable = false)
+	private String estado;
+	
+	@Column(nullable = false)
+	private String cep;
+	
+	@Column(nullable = false)
+	private String pais;
+
+}
