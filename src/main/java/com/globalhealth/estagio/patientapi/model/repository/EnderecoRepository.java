bdbd83abf4b82e2cb9b1f1c33e3c5b5941902a1e
TicianoFilho <ticianofilho@gmail.com>
2022-11-26 13:47:35 -0300

    refactor: change some classes name

--- src/main/java/com/globalhealth/estagio/patientapi/model/repository/EnderecoRepository.java
+++ src/main/java/com/globalhealth/estagio/patientapi/model/repository/EnderecoRepository.java
@@ -3,9 +3,9 @@ package com.globalhealth.estagio.patientapi.model.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.globalhealth.estagio.patientapi.model.entity.Endereco;
+import com.globalhealth.estagio.patientapi.model.entity.EnderecoEntity;
 
 @Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+public interface EnderecoRepository extends JpaRepository<EnderecoEntity, Long> {
 
 }
