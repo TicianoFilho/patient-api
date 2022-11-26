92b90cae4f7831da7078a8afe674952a44b7e35c
TicianoFilho <ticianofilho@gmail.com>
2022-11-26 15:48:43 -0300

    refactor: update project

--- src/main/java/com/globalhealth/estagio/patientapi/dto/DependenteDto.java
+++ src/main/java/com/globalhealth/estagio/patientapi/dto/DependenteDto.java
@@ -1,5 +1,22 @@
 package com.globalhealth.estagio.patientapi.dto;
 
+import com.globalhealth.estagio.patientapi.model.entity.DependenteEntity;
+
 public class DependenteDto extends Paciente {
 
+	public static DependenteEntity toEntity(DependenteDto dto) {
+		DependenteEntity entity = new DependenteEntity();	
+		entity.setNome(dto.getNome());
+		entity.setCpf(dto.getCpf());
+		entity.setCodigoCartao(dto.getCodigoCartao());
+		return entity;
+	}
+	
+	public static DependenteDto toDto(DependenteEntity entity) {
+		DependenteDto dto = new DependenteDto();
+		dto.setNome(entity.getNome());
+		dto.setCpf(entity.getCpf());
+		dto.setCodigoCartao(entity.getCodigoCartao());
+		return dto;
+	}
 }
