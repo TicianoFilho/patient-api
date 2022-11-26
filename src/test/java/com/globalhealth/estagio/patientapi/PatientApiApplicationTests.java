da949752edf1cb35086706697eb0672c00b1c77b
TicianoFilho <ticianofilho@gmail.com>
2022-11-26 10:23:33 -0300

    first commit. All project structure was created and database connection was done in application.properties

---
+++ src/test/java/com/globalhealth/estagio/patientapi/PatientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalhealth.estagio.patientapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
