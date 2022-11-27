77c894d7b13c571b0d06a9cf0555b41af1e0bd28
TicianoFilho <ticianofilho@gmail.com>
2022-11-27 15:15:35 -0300

    feat: add TitularRepository

---
+++ src/main/java/com/globalhealth/estagio/patientapi/repository/TitularRepository.java
@@ -0,0 +1,9 @@
+package com.globalhealth.estagio.patientapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.globalhealth.estagio.patientapi.entity.TitularEntity;
+
+public interface TitularRepository extends JpaRepository<TitularEntity, Long>{
+
+}
