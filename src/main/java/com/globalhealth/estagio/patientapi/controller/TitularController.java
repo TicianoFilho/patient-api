6f4abd8ec3167adfdc9a0980f7931330379a763f
TicianoFilho <ticianofilho@gmail.com>
2022-11-27 15:36:03 -0300

    feat: add findAll method in TitularController

--- src/main/java/com/globalhealth/estagio/patientapi/controller/TitularController.java
+++ src/main/java/com/globalhealth/estagio/patientapi/controller/TitularController.java
@@ -1,8 +1,12 @@
 package com.globalhealth.estagio.patientapi.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.globalhealth.estagio.patientapi.dto.TitularDto;
 import com.globalhealth.estagio.patientapi.service.TitularService;
 
 @RestController
@@ -15,5 +19,8 @@ public class TitularController {
 		this.titularService = titularService;
 	}
 	
-
+	@GetMapping
+	public List<TitularDto> findAll() {
+		return titularService.findAll();
+	}
 }
