4f358eaa17d5915e1ac45a2c57f161d3c67e6220
TicianoFilho <ticianofilho@gmail.com>
2022-11-27 16:53:36 -0300

    feat: add findAll method implementation in TitularServiceImpl

--- src/main/java/com/globalhealth/estagio/patientapi/service/impl/TitularServiceImpl.java
+++ src/main/java/com/globalhealth/estagio/patientapi/service/impl/TitularServiceImpl.java
@@ -1,14 +1,14 @@
 package com.globalhealth.estagio.patientapi.service.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import com.globalhealth.estagio.patientapi.dto.DependenteDto;
 import com.globalhealth.estagio.patientapi.dto.EnderecoDto;
 import com.globalhealth.estagio.patientapi.dto.TitularDto;
 import com.globalhealth.estagio.patientapi.entity.TitularEntity;
-import com.globalhealth.estagio.patientapi.entity.DependenteEntity;
 import com.globalhealth.estagio.patientapi.entity.EnderecoEntity;
 import com.globalhealth.estagio.patientapi.repository.TitularRepository;
 import com.globalhealth.estagio.patientapi.service.TitularService;
@@ -17,9 +17,11 @@ import com.globalhealth.estagio.patientapi.service.TitularService;
 public class TitularServiceImpl implements TitularService {
 
 	private TitularRepository titularRepository;
+	private ModelMapper modelMapper;
 	
-	public TitularServiceImpl(TitularRepository titularRepository) {
+	public TitularServiceImpl(TitularRepository titularRepository, ModelMapper modelMapper) {
 		this.titularRepository = titularRepository;
+		this.modelMapper = modelMapper;
 	}
 
 	@Override
@@ -30,8 +32,8 @@ public class TitularServiceImpl implements TitularService {
 
 	@Override
 	public List<TitularDto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<TitularEntity> titulares = titularRepository.findAll();
+		return titulares.stream().map(titular -> this.toDto(titular)).collect(Collectors.toList());
 	}
 
 	@Override
@@ -53,49 +55,21 @@ public class TitularServiceImpl implements TitularService {
 	}
 	
 	private TitularEntity toEntity(TitularDto dto) {
-		TitularEntity entity = new TitularEntity();	
-		entity.setNome(dto.getNome());
-		entity.setCpf(dto.getCpf());
-		entity.setCodigoCartao(dto.getCodigoCartao());
-		entity.setEndereco(this.toEnderecoEntity(dto.getEndereco()));
-		entity.setDependentes(this.toDependenteEntity(dto.getDependentes()));
+		TitularEntity entity = modelMapper.map(dto, TitularEntity.class);
 		return entity;
-	}
-	
-	private List<DependenteEntity> toDependenteEntity(List<DependenteDto> dependentes) {
-
-		return null;
 	}
 
 	private TitularDto toDto(TitularEntity entity) {
-		TitularDto dto = new TitularDto();
-		dto.setId(entity.getId());
-		dto.setNome(entity.getNome());
-		dto.setCpf(entity.getCpf());
-		dto.setCodigoCartao(entity.getCodigoCartao());
-		dto.setEndereco(this.toEnderecoDto(entity.getEndereco()));
+		TitularDto dto = modelMapper.map(entity, TitularDto.class);
 		return dto;
 	}
 	
 	public EnderecoEntity toEnderecoEntity(EnderecoDto dto) {
-		EnderecoEntity entity = new EnderecoEntity();
-		entity.setCep(dto.getCep());
-		entity.setRua(dto.getRua());
-		entity.setBairro(dto.getBairro());
-		entity.setCidade(dto.getCidade());
-		entity.setEstado(dto.getEstado());
-		entity.setPais(dto.getPais());
+		EnderecoEntity entity = modelMapper.map(dto, EnderecoEntity.class);
 		return entity;
 	}
 	public EnderecoDto toEnderecoDto(EnderecoEntity entity) {
-		EnderecoDto dto = new EnderecoDto();
-		dto.setId(entity.getId());
-		dto.setCep(entity.getCep());
-		dto.setRua(entity.getRua());
-		dto.setBairro(entity.getBairro());
-		dto.setCidade(entity.getCidade());
-		dto.setEstado(entity.getEstado());
-		dto.setPais(entity.getPais());
+		EnderecoDto dto = modelMapper.map(entity, EnderecoDto.class);
 		return dto;
 	}
 
