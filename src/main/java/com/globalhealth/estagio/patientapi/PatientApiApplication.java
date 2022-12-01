24c1ffee38e77d4184212ff4c48c2ead0557375d
TicianoFilho <ticianofilho@gmail.com>
2022-12-01 19:02:52 -0300

    refactor: create package config and configuration classes with @Bean annotation

--- src/main/java/com/globalhealth/estagio/patientapi/PatientApiApplication.java
+++ src/main/java/com/globalhealth/estagio/patientapi/PatientApiApplication.java
@@ -1,24 +1,10 @@
 package com.globalhealth.estagio.patientapi;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PatientApiApplication {
-
-	@Bean
-	public ModelMapper modelMapper() {                  // convert entity to dto and vice-versa
-		return new ModelMapper();
-	}
-	
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {                // for user password
-		return new BCryptPasswordEncoder();
-	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApiApplication.class, args);
